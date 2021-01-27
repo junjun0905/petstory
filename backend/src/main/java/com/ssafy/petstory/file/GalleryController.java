849fdb82cfe66589c4c184e35c7034d12aa3accc
geonhee ryu <sadog94@naver.com>
2021-01-28 03:28:09 +0900

    S04P12B204-145 [feat] : BE - AWS S3 이미지 조회 테스트 -> [90] feat - 로그인 기능 추가
    -> 로그인 시 썸네일 선택해서 해당 프로필로 로그인

--- backend/src/main/java/com/ssafy/petstory/file/GalleryController.java
+++ backend/src/main/java/com/ssafy/petstory/file/GalleryController.java
@@ -2,11 +2,13 @@ package com.ssafy.petstory.file;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -15,7 +17,10 @@ public class GalleryController {
     private GalleryService galleryService;
 
     @GetMapping("/gallery")
-    public String dispWrite() {
+    public String dispWrite(Model model) {
+        List<GalleryDto> galleryDtoList = galleryService.getList();
+
+        model.addAttribute("galleryList", galleryDtoList);
 
         return "/gallery";
     }
--- backend/src/main/java/com/ssafy/petstory/file/GalleryDto.java
+++ backend/src/main/java/com/ssafy/petstory/file/GalleryDto.java
@@ -10,6 +10,7 @@ public class GalleryDto {
     private Long id;
     private String title;
     private String filePath;
+    private String imgFullPath;
 
     public GalleryEntity toEntity(){
         GalleryEntity build = GalleryEntity.builder()
@@ -21,9 +22,10 @@ public class GalleryDto {
     }
 
     @Builder
-    public GalleryDto(Long id, String title, String filePath) {
+    public GalleryDto(Long id, String title, String filePath, String imgFullPath) {
         this.id = id;
         this.title = title;
         this.filePath = filePath;
+        this.imgFullPath = imgFullPath;
     }
 }
