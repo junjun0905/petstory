d040777a8a67092fad901007839c9cabf6cc08d7
유진우 <phoenix9373@naver.com>
2021-01-29 01:09:40 +0900

    S04P12B204-132 [feat] : FE - 123 이슈 수정, router

--- frontend/pet/src/app.jsx
+++ frontend/pet/src/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './app.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './views/LandingPage/LandingPage';
 import LoginPage from './views/LoginPage/LoginPage';
 import RegisterPage from './views/RegisterPage/RegisterPage';
 import NavBar from './views/NavBar/NavBar';
+import MainPage from './views/MainPage/MainPage';
 // import Auth from './hoc/auth';
 
 function App() {
@@ -16,9 +16,7 @@ function App() {
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/register" component={RegisterPage} />
-          {/* <Route exact path="/" component={Auth(landingPage, null)} />
-          <Route exact path="/login" component={Auth(loginPage, false)} />
-          <Route exact path="/register" component={Auth(registerPage, false)} /> */}
+          <Route exact path="/main" component={MainPage} />
         </Switch>
       </div>
     </Router>
